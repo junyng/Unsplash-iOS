2a2512f7d1cd8be133f1b811258ab6a80eabf23b
junyng <pom0894@naver.com>
2020-10-22 12:21:58 +0900

    add initial project

---
+++ Unsplash/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Unsplash
+//
+//  Created by Cho Junyeong on 2020/10/22.
+//  Copyright © 2020 junyeong-cho. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+}
+
