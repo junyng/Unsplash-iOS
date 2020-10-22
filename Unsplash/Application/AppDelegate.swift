778e7388098d6a2e50a1237b2ae591dd18af55ce
junyng <pom0894@naver.com>
2020-10-22 15:37:57 +0900

    chore: set proejct folder structure

---
+++ Unsplash/Application/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Unsplash
+//
+//  Created by Cho Junyeong on 2020/10/22.
+//  Copyright © 2020 junyeong-cho. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+}
+
