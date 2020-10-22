6fa77e40922826a5cb1d822723a913af593bc36e
junyng <pom0894@naver.com>
2020-10-23 00:11:32 +0900

    feat: add FeedViewController

---
+++ Unsplash/Source/Scene/Feed/FeedViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FeedViewController.swift
+//  Unsplash
+//
+//  Created by Cho Junyeong on 2020/10/22.
+//  Copyright © 2020 junyeong-cho. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    @IBOutlet private weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
