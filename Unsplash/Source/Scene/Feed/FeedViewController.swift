0c3c2d35828be6ebea7595634845917b3ef72954
junyng <pom0894@naver.com>
2020-10-23 13:57:31 +0900

    feat: add PhotoCell to photo list (#5)

--- Unsplash/Source/Scene/Feed/FeedViewController.swift
+++ Unsplash/Source/Scene/Feed/FeedViewController.swift
@@ -15,6 +15,23 @@ class FeedViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        collectionView.register(PhotoCell.self, forCellWithReuseIdentifier: PhotoCell.identifier)
     }
 
+}
+
+extension FeedViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCell.identifier, for: indexPath) as? PhotoCell else {
+            return UICollectionViewCell()
+        }
+        
+        return cell
+    }
+    
+    
 }
---
+++ Unsplash/Source/Scene/Feed/PhotoCell.swift
@@ -0,0 +1,38 @@
+//
+//  PhotoCell.swift
+//  Unsplash
+//
+//  Created by Cho Junyeong on 2020/10/23.
+//  Copyright © 2020 junyeong-cho. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+    static let identifier = "PhotoCell"
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = UIColor.green
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(imageView)
+        
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            imageView.topAnchor.constraint(equalTo: topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
