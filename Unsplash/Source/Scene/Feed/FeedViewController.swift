8655aabe76887cf37c7cf1287c83567eda7c8372
junyng <pom0894@naver.com>
2020-10-31 16:30:58 +0900

    feat: add PhotoService to fetch photos from api

--- Unsplash/Source/Scene/Feed/FeedViewController.swift
+++ Unsplash/Source/Scene/Feed/FeedViewController.swift
@@ -9,42 +9,45 @@
 import UIKit
 
 class FeedViewController: UIViewController {
-
+    
     @IBOutlet private weak var collectionView: UICollectionView!
     
-    let imageNames: [String] = ["image1", "image2", "image3"]
+    private let photoService = PhotoService(networking: Networking<Unsplash>())
+    private var photos = [Photo]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         collectionView.register(PhotoCell.self, forCellWithReuseIdentifier: PhotoCell.identifier)
         
         if let layout = collectionView.collectionViewLayout as? FeedLayout {
             layout.delegate = self
         }
     }
-
-    func getImage(named name: String) -> UIImage? {
-        guard let path = Bundle.main.path(forResource: name, ofType: ".jpg") else {
-            return nil
-        }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
         
-        return UIImage(contentsOfFile: path)
+        photoService.fetchPhotos { (result) in
+            if case let .success(photos) = result {
+                self.photos = photos
+                DispatchQueue.main.async {
+                    self.collectionView.reloadData()
+                }
+            }
+        }
     }
 }
 
 extension FeedViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return imageNames.count
+        return photos.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCell.identifier, for: indexPath) as? PhotoCell,
-            let image = getImage(named: imageNames[indexPath.row]) else {
-                return UICollectionViewCell()
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCell.identifier, for: indexPath) as? PhotoCell else {
+            return UICollectionViewCell()
         }
-        
-        cell.configure(image: image)
         
         return cell
     }
@@ -52,10 +55,6 @@ extension FeedViewController: UICollectionViewDataSource {
 
 extension FeedViewController: FeedLayoutDelegate {
     func collectionView(_ collectionView: UICollectionView, heightForPhotoAtIndexPath indexPath: IndexPath) -> CGFloat {
-        guard let image = getImage(named: imageNames[indexPath.row]) else {
-            return 0
-        }
-        
-        return image.size.height * (UIScreen.main.bounds.size.width / image.size.width)
+        return 100
     }
 }
