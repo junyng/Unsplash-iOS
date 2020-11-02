5234ba89fd095b221d3b67bfb65f3269a7b744d4
junyng <pom0894@naver.com>
2020-11-03 00:14:44 +0900

    feat: add navigation title when loading photo info

--- Unsplash/Source/Scene/PhotoDetail/PhotoDetailViewController.swift
+++ Unsplash/Source/Scene/PhotoDetail/PhotoDetailViewController.swift
@@ -99,9 +99,12 @@ class PhotoDetailViewController: UIViewController {
     
     private func loadPhotoInfo() {
         guard let indexPath = collectionView.indexPathsForVisibleItems.first,
-            let photoID = photos?[indexPath.item].id else {
+            let photoID = photos?[indexPath.item].id,
+            let title = photos?[indexPath.item].user?.fullName else {
                 return
         }
+        
+        navigationItem.title = title
         photoInfoButton.loading(true)
         photoService.fetchPhoto(photoID: photoID) { (result) in
             if case let .success(photo) = result {
